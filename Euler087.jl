f8b888b7bfbc9a90f3ae7813871171f9415802f6
Arnaud Amiel <aamiel@gmail.com>
2014-08-22 09:08:26 +0100

    Improvements to Euler087 to run under 1s
    Reduced search space and various optimisations

--- Euler087.jl
+++ Euler087.jl
@@ -1,7 +1,9 @@
 #= Prime power triples
 Problem 87
 
-The smallest number expressible as the sum of a prime square, prime cube, and prime fourth power is 28. In fact, there are exactly four numbers below fifty that can be expressed in such a way:
+The smallest number expressible as the sum of a prime square, prime cube, and prime fourth power is 28. In fact, there are exactly four numbers below fifty 
+
+that can be expressed in such a way:
  
 28 = 2^2 + 2^3 + 2^4
 33 = 3^2 + 2^3 + 2^4
@@ -12,40 +14,50 @@ How many numbers below fifty million can be expressed as the sum of a prime squa
 
 =#
 
+function primeCount(n::Int)
+
+    ifloor(1.25506 * n / log(n))
+
+end
+
 function solve087()
 
     MAX = 50000000
 
-    myPrimes = primes(ifloor(sqrt(MAX)))
+    maxSquares = ifloor(sqrt(MAX))
+    maxCubes = ifloor(cbrt(MAX))
+    maxFourth = ifloor(MAX^(1/4))
+
+    myPrimes = primes(maxSquares)
 
     mySquares = Array(Int,length(myPrimes))
-    myCubes = Array(Int,length(myPrimes))
-    myFourth = Array(Int,length(myPrimes))
+    myCubes = Array(Int,primeCount(maxCubes))
+    myFourth = Array(Int,primeCount(maxFourth))
 
-    for i = 1:length(myPrimes)
-        mySquares[i] = myPrimes[i]*myPrimes[i]
-        myCubes[i] = mySquares[i]*myPrimes[i]
-        myFourth[i] = mySquares[i]*mySquares[i]
+    for i = 1:length(mySquares)
+        @inbounds mySquares[i] = myPrimes[i]*myPrimes[i]
+    end
+
+    for i = 1:length(myCubes)
+        @inbounds myCubes[i] = mySquares[i]*myPrimes[i]
+    end
+
+    for i = 1:length(myFourth)
+        @inbounds myFourth[i] = mySquares[i]*mySquares[i]
     end
 
     isSum = falses(MAX)
 
     for a in mySquares, b in myCubes, c in myFourth
         if a+b+c < MAX
-            isSum[a+b+c] = true
+            @inbounds isSum[a+b+c] = true
         end
     end
 
-    counter = 0
-    for i in isSum
-        if i
-            counter += 1
-        end
-    end
-
-    counter
+    sum(isSum)
 
 end
 
 using Base.Test
-@test solve087() == 1097343
+
+#@test solve087() == 1097343
