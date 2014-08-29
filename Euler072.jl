ba24900dbf88ec871c9b0919628ff12096de5d94
Arnaud Amiel <aamiel@gmail.com>
2014-08-29 11:56:36 +0100

    Added solution to problem 72

---
+++ Euler072.jl
@@ -0,0 +1,41 @@
+#= Counting fractions
+Problme 72
+
+Consider the fraction, n/d, where n and d are positive integers. If n≤d and GCD(n,d)=1, it is called a reduced proper fraction.
+ 
+If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+ 
+1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+ 
+It can be seen that there are 21 elements in this set.
+ 
+How many elements would be contained in the set of reduced proper fractions for d ≤ 1,000,000?
+
+=#
+
+function phi(n::Int)
+
+    counter = n
+
+    for p in keys(factor(n))
+        counter *= 1 -1/p
+    end
+
+    counter
+
+end
+
+function solve072()
+
+    counter = 0
+
+    for i = 2:1000000
+        counter += phi(i)
+    end
+
+    counter
+
+end
+
+using Base.Test
+@test solve072() == 303963552391
